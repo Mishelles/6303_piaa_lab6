c4f498b05a47d28501baaadd92dd664f6b663abe
root <root@ubuntu.ruvds.com>
2018-05-30 17:07:32 +0300

    Cyrillic support

--- test/testPatternsSearch.cpp
+++ test/testPatternsSearch.cpp
@@ -59,19 +59,12 @@ TEST_P(patternsSearchTest, searchPatternsInText) {
 
 INSTANTIATE_TEST_CASE_P(Default, patternsSearchTest,
   testing::Values(
-<<<<<<< HEAD
   testState{L"CCCA", 1, {L"CC"}, {{1, 1}, {2, 1}}},
   testState{L"CCCA", 1, {L"ACC"}, {}},
   testState{L"ACIRDJHRDLJSDNCJOPQLMZ", 5, {L"ACI", L"HRLD", L"L", L"OPQ", L"DN"}, {{1, 1}, {10, 3}, {13, 5}, {17, 4}, {20, 3}}},
   testState{L"LOLYPLOLYPOPLOL", 5, {L"LOL", L"LOLY", L"LOLYP", L"LOLYPOP", L"L"}, {{1, 1}, {1, 2}, {1, 3}, {1, 5}, {3, 5}, {6, 1}, {6, 2}, {6, 3}, {6, 4}, {6, 5}, {8, 5}, {13, 1}, {13, 5}, {15, 5}}},
   // Update: cyrillic symbols
   testState{L"CФCЙ", 1, {L"Й"}, {{4, 1}}}
-=======
-  testState{"CCCA", 1, {"CC"}, {{1, 1}, {2, 1}}},
-  testState{"CCCA", 1, {"ACC"}, {}},
-  testState{"ACIRDJHRDLJSDNCJOPQLMZ", 5, {"ACI", "HRLD", "L", "OPQ", "DN"}, {{1, 1}, {10, 3}, {13, 5}, {17, 4}, {20, 3}}},
-  testState{"LOLYPLOLYPOPLOL", 5, {"LOL", "LOLY", "LOLYP", "LOLYPOP", "L"}, {{1, 1}, {1, 2}, {1, 3}, {1, 5}, {3, 5}, {6, 1}, {6, 2}, {6, 3}, {6, 4}, {6, 5}, {8, 5}, {13, 1}, {13, 5}, {15, 5}}}
->>>>>>> 3c164f495dc5b48a221e3a6da5a0505f5e037ee8
   ));
 
 int main(int argc, char* argv[]) {
